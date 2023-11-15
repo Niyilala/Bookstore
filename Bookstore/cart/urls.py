23469564f479aa68fcc02df1478acaa326ee573a
Niyilala <adeniyi.oluwajana@gmail.com>
2023-11-15 05:47:24 +0100

    'update'

--- Bookstore/cart/urls.py
+++ Bookstore/cart/urls.py
@@ -5,3 +5,17 @@ from . import views
 
 
 urlpatterns = [
+
+path('', views.cart_summary, name = 'cart-summary'),
+
+
+path('add/', views.cart_add, name = 'cart-add'),
+
+
+path('delete', views.cart_delete, name = 'cart-delete'),
+
+
+path('', views.cart_update, name = 'cart-update'),
+
+
+]
